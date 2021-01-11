ffd97e75035647d866ad721db5646cb6feac074d
tadejdelopst <tadejdelopst@gmail.com>
2021-01-11 13:01:32 +0100

    Baza

--- Form1.cs
+++ Form1.cs
@@ -7,9 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+using System.Configuration;
 
 namespace miniProjekt___Avtosole {
     public partial class Form1 : Form {
+        string connect = baza.connect();
         public Form1() {
             InitializeComponent();
         }
---
+++ baza.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Configuration;
+
+namespace miniProjekt___Avtosole {
+    public class baza {
+
+        public static string connect() {
+            var uriString = ConfigurationManager.AppSettings["ELEPHANTSQL_URL"] ?? "postgres://ozgwcyjg:XRaflBkA9NYPoFjJsGj7WbrzrjJX9sYV@kandula.db.elephantsql.com:5432/ozgwcyjg";
+            var uri = new Uri(uriString);
+            var db = uri.AbsolutePath.Trim('/');
+            var user = uri.UserInfo.Split(':')[0];
+            var passwd = uri.UserInfo.Split(':')[1];
+            var port = uri.Port > 0 ? uri.Port : 5432;
+            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+            uri.Host, db, user, passwd, port);
+            return connStr;
+        }
+
+    }
+}
